f0d85c5bd13899579258190873205e53810308ef
ProfAristi <profesor.aristimuno@gmail.com>
2024-07-10 23:01:18 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,455 @@
+//=====[Libraries]=============================================================
+
 #include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+#define NUMBER_OF_KEYS                           4
+#define TIME_INCREMENT_MS                       10
+#define DEBOUNCE_KEY_TIME_MS                    40
+#define KEYPAD_NUMBER_OF_ROWS                    4
+#define KEYPAD_NUMBER_OF_COLS                    4
+#define EVENT_MAX_STORAGE                      100
+#define EVENT_NAME_MAX_LENGTH                   14
+
+//=====[Declaration of public data types]======================================
+
+typedef enum {
+    MATRIX_KEYPAD_SCANNING,
+    MATRIX_KEYPAD_DEBOUNCE,
+    MATRIX_KEYPAD_KEY_HOLD_PRESSED
+} matrixKeypadState_t;
+
+typedef struct systemEvent {
+    time_t seconds;
+    char typeOfEvent[EVENT_NAME_MAX_LENGTH];
+} systemEvent_t;
+
+//=====[Declaration and initialization of public global objects]===============
+
+/*
+DigitalIn I1();
+DigitalIn I2();
+DigitalIn I3();
+DigitalIn I4();
+DigitalIn I5();
+DigitalIn I6();
+DigitalIn I7();
+DigitalIn I8();
+
+DigitalOut Q1();
+DigitalOut Q2();
+DigitalOut Q3();
+DigitalOut Q4();
+*/
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+
+
+DigitalOut keypadRowPins[KEYPAD_NUMBER_OF_ROWS] = {PB_3, PB_5, PC_7, PA_15};
+DigitalIn keypadColPins[KEYPAD_NUMBER_OF_COLS]  = {PB_12, PB_13, PB_15, PC_6};
+
+//=====[Declaration and initialization of public global variables]=============
+
+int numberOfIncorrectCodes = 0;
+int numberOfHashKeyReleasedEvents = 0;
+int keyBeingCompared    = 0;
+int keyBeingConfigured  = 0;
+char codeSequence[NUMBER_OF_KEYS]   = { '1', '8', '0', '5' };
+char keysPressed[NUMBER_OF_KEYS] = { '0', '0', '0', '0' };
+int accumulatedTimeAlarm = 0;
+
+bool alarmLastState        = OFF;
+bool gasLastState          = OFF;
+bool tempLastState         = OFF;
+bool ICLastState           = OFF;
+bool SBLastState           = OFF;
+
+bool gasDetectorState          = OFF;
+bool overTempDetectorState     = OFF;
+
+
+int accumulatedDebounceMatrixKeypadTime = 0;
+int matrixKeypadCodeIndex = 0;
+char matrixKeypadLastKeyPressed = '\0';
+char matrixKeypadIndexToCharArray[] = {
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D',
+};
+matrixKeypadState_t matrixKeypadState;
+
+int eventsIndex            = 0;
+systemEvent_t arrayOfStoredEvents[EVENT_MAX_STORAGE];
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void uartTask();
+void availableCommands();
+bool areEqual();
+
+void eventLogUpdate();
+void systemElementStateUpdate( bool lastState,
+                               bool currentState,
+                               const char* elementName );
+
+void matrixKeypadInit();
+char matrixKeypadScan();
+char matrixKeypadUpdate();
+
+//=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    DigitalIn B1_USER(BUTTON1);
+    inputsInit();
+    outputsInit();
+    /*while (true) {
+       
+    }*/
+}
 
-    DigitalOut LD1(LED1);
+//=====[Implementations of public functions]===================================
 
-    while (true) {
-        LD1 = B1_USER;
+void inputsInit()
+{
+ matrixKeypadInit();
+}
+
+void outputsInit()
+{
+   
+     
+}
+
+                            
+                    
+ 
+
+void uartTask()
+{
+    char receivedChar = '\0';
+    char str[100];
+    int stringLength;
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+        switch (receivedChar) {
+        case '1':
+            if ( alarmState ) {
+                uartUsb.write( "The alarm is activated\r\n", 24);
+            } else {
+                uartUsb.write( "The alarm is not activated\r\n", 28);
+            }
+            break;
+
+        case '2':
+            if ( !mq2 ) {
+                uartUsb.write( "Gas is being detected\r\n", 22);
+            } else {
+                uartUsb.write( "Gas is not being detected\r\n", 27);
+            }
+            break;
+
+        case '3':
+            if ( overTempDetector ) {
+                uartUsb.write( "Temperature is above the maximum level\r\n", 40);
+            } else {
+                uartUsb.write( "Temperature is below the maximum level\r\n", 40);
+            }
+            break;
+            
+        case '4':
+            uartUsb.write( "Please enter the four digits numeric code ", 42 );
+            uartUsb.write( "to deactivate the alarm: ", 25 );
+
+            incorrectCode = false;
+
+            for ( keyBeingCompared = 0;
+                  keyBeingCompared < NUMBER_OF_KEYS;
+                  keyBeingCompared++) {
+                uartUsb.read( &receivedChar, 1 );
+                uartUsb.write( "*", 1 );
+                if ( codeSequence[keyBeingCompared] != receivedChar ) {
+                    incorrectCode = true;
+                }
+            }
+
+            if ( incorrectCode == false ) {
+                uartUsb.write( "\r\nThe code is correct\r\n\r\n", 25 );
+                alarmState = OFF;
+                incorrectCodeLed = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                uartUsb.write( "\r\nThe code is incorrect\r\n\r\n", 27 );
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes++;
+            }
+            break;
+
+        case '5':
+            uartUsb.write( "Please enter the new four digits numeric code ", 46 );
+            uartUsb.write( "to deactivate the alarm: ", 25 );
+
+            for ( keyBeingConfigured = 0;
+                  keyBeingConfigured < NUMBER_OF_KEYS;
+                  keyBeingConfigured++) {
+                uartUsb.read( &receivedChar, 1 );
+                uartUsb.write( "*", 1 );
+                codeSequence[keyBeingConfigured] = receivedChar;
+            }
+            
+            uartUsb.write( "\r\nNew code generated\r\n\r\n", 24 );
+            break;
+
+        case 'c':
+        case 'C':
+            sprintf ( str, "Temperature: %.2f \xB0 C\r\n", lm35TempC );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
+
+        case 'f':
+        case 'F':
+            sprintf ( str, "Temperature: %.2f \xB0 F\r\n", 
+                celsiusToFahrenheit( lm35TempC ) );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
+
+        case 's':
+        case 'S':
+            struct tm rtcTime;
+            int strIndex;
+                    
+            uartUsb.write( "\r\nType four digits for the current year (YYYY): ", 48 );
+            for( strIndex=0; strIndex<4; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[4] = '\0';
+            rtcTime.tm_year = atoi(str) - 1900;
+            uartUsb.write( "\r\n", 2 );
+
+            uartUsb.write( "Type two digits for the current month (01-12): ", 47 );
+            for( strIndex=0; strIndex<2; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[2] = '\0';
+            rtcTime.tm_mon  = atoi(str) - 1;
+            uartUsb.write( "\r\n", 2 );
+
+            uartUsb.write( "Type two digits for the current day (01-31): ", 45 );
+            for( strIndex=0; strIndex<2; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[2] = '\0';
+            rtcTime.tm_mday = atoi(str);
+            uartUsb.write( "\r\n", 2 );
+
+            uartUsb.write( "Type two digits for the current hour (00-23): ", 46 );
+            for( strIndex=0; strIndex<2; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[2] = '\0';
+            rtcTime.tm_hour = atoi(str);
+            uartUsb.write( "\r\n", 2 );
+
+            uartUsb.write( "Type two digits for the current minutes (00-59): ", 49 );
+            for( strIndex=0; strIndex<2; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[2] = '\0';
+            rtcTime.tm_min  = atoi(str);
+            uartUsb.write( "\r\n", 2 );
+
+            uartUsb.write( "Type two digits for the current seconds (00-59): ", 49 );
+            for( strIndex=0; strIndex<2; strIndex++ ) {
+                uartUsb.read( &str[strIndex] , 1 );
+                uartUsb.write( &str[strIndex] ,1 );
+            }
+            str[2] = '\0';
+            rtcTime.tm_sec  = atoi(str);
+            uartUsb.write( "\r\n", 2 );
+
+            rtcTime.tm_isdst = -1;
+            set_time( mktime( &rtcTime ) );
+            uartUsb.write( "Date and time has been set\r\n", 28 );
+
+            break;
+                        
+        case 't':
+        case 'T':
+            time_t epochSeconds;
+            epochSeconds = time(NULL);
+            sprintf ( str, "Date and Time = %s", ctime(&epochSeconds));
+            uartUsb.write( str , strlen(str) );
+            uartUsb.write( "\r\n", 2 );
+            break;
+
+        case 'e':
+        case 'E':
+            for (int i = 0; i < eventsIndex; i++) {
+                sprintf ( str, "Event = %s\r\n", 
+                    arrayOfStoredEvents[i].typeOfEvent);
+                uartUsb.write( str , strlen(str) );
+                sprintf ( str, "Date and Time = %s\r\n",
+                    ctime(&arrayOfStoredEvents[i].seconds));
+                uartUsb.write( str , strlen(str) );
+                uartUsb.write( "\r\n", 2 );
+            }
+            break;
+
+        default:
+            availableCommands();
+            break;
+
+        }
     }
+}
+
+void availableCommands()
+{
+    uartUsb.write( "Available commands:\r\n", 21 );
+    uartUsb.write( "Press '1' NN\r\n", 34 );
+    uartUsb.write( "Press '2' NN\r\n", 41 );
+    uartUsb.write( "Press '3' NN\r\n", 54 );
+    uartUsb.write( "Press '4'NN\r\n", 38 );
+    uartUsb.write( "Press '5' to enter a new code\r\n", 31 );
+    uartUsb.write( "Press 'f' or 'F' to NN\r\n", 52 );
+    uartUsb.write( "Press 'c' or 'C' to NN\r\n", 49 );
+    uartUsb.write( "Press 's' or 'S' to NN\r\n", 43 );
+    uartUsb.write( "Press 't' or 'T' to NN\r\n", 43 );
+    uartUsb.write( "Press 'e' or 'E' toNN\r\n\r\n", 45 );
+}
+
+bool areEqual()
+{
+    int i;
+
+    for (i = 0; i < NUMBER_OF_KEYS; i++) {
+        if (codeSequence[i] != keysPressed[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void systemElementStateUpdate( bool lastState,
+                               bool currentState,
+                               const char* elementName )
+{
+    char eventAndStateStr[EVENT_NAME_MAX_LENGTH] = "";
+
+    if ( lastState != currentState ) {
+
+        strcat( eventAndStateStr, elementName );
+        if ( currentState ) {
+            strcat( eventAndStateStr, "_ON" );
+        } else {
+            strcat( eventAndStateStr, "_OFF" );
+        }
+
+        arrayOfStoredEvents[eventsIndex].seconds = time(NULL);
+        strcpy( arrayOfStoredEvents[eventsIndex].typeOfEvent,eventAndStateStr );
+        if ( eventsIndex < EVENT_MAX_STORAGE - 1 ) {
+            eventsIndex++;
+        } else {
+            eventsIndex = 0;
+        }
+
+        uartUsb.write( eventAndStateStr , strlen(eventAndStateStr) );
+        uartUsb.write( "\r\n", 2 );
+    }
+}
+
+
+}
+
+void matrixKeypadInit()
+{
+    matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+    int pinIndex = 0;
+    for( pinIndex=0; pinIndex<KEYPAD_NUMBER_OF_COLS; pinIndex++ ) {
+        (keypadColPins[pinIndex]).mode(PullUp);
+    }
+}
+
+char matrixKeypadScan()
+{
+    int row = 0;
+    int col = 0;
+    int i = 0;
+
+    for( row=0; row<KEYPAD_NUMBER_OF_ROWS; row++ ) {
+
+        for( i=0; i<KEYPAD_NUMBER_OF_ROWS; i++ ) {
+            keypadRowPins[i] = ON;
+        }
+
+        keypadRowPins[row] = OFF;
+
+        for( col=0; col<KEYPAD_NUMBER_OF_COLS; col++ ) {
+            if( keypadColPins[col] == OFF ) {
+                return matrixKeypadIndexToCharArray[row*KEYPAD_NUMBER_OF_ROWS + col];
+            }
+        }
+    }
+    return '\0';
+}
+
+char matrixKeypadUpdate()
+{
+    char keyDetected = '\0';
+    char keyReleased = '\0';
+
+    switch( matrixKeypadState ) {
+
+    case MATRIX_KEYPAD_SCANNING:
+        keyDetected = matrixKeypadScan();
+        if( keyDetected != '\0' ) {
+            matrixKeypadLastKeyPressed = keyDetected;
+            accumulatedDebounceMatrixKeypadTime = 0;
+            matrixKeypadState = MATRIX_KEYPAD_DEBOUNCE;
+        }
+        break;
+
+    case MATRIX_KEYPAD_DEBOUNCE:
+        if( accumulatedDebounceMatrixKeypadTime >=
+            DEBOUNCE_KEY_TIME_MS ) {
+            keyDetected = matrixKeypadScan();
+            if( keyDetected == matrixKeypadLastKeyPressed ) {
+                matrixKeypadState = MATRIX_KEYPAD_KEY_HOLD_PRESSED;
+            } else {
+                matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+            }
+        }
+        accumulatedDebounceMatrixKeypadTime =
+            accumulatedDebounceMatrixKeypadTime + TIME_INCREMENT_MS;
+        break;
+
+    case MATRIX_KEYPAD_KEY_HOLD_PRESSED:
+        keyDetected = matrixKeypadScan();
+        if( keyDetected != matrixKeypadLastKeyPressed ) {
+            if( keyDetected == '\0' ) {
+                keyReleased = matrixKeypadLastKeyPressed;
+            }
+            matrixKeypadState = MATRIX_KEYPAD_SCANNING;
+        }
+        break;
+
+    default:
+        matrixKeypadInit();
+        break;
+    }
+    return keyReleased;
 }
